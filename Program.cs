6c36cf8512d6290c1139cf47891d8e6c2d8c90ab
Aida Radfard <aida.radfard.1@gmail.com>
2025-07-06 01:51:55 +0330

    Task Manager

---
+++ Program.cs
@@ -0,0 +1,35 @@
+
+namespace TaskManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+/*            builder.Services.AddDbContext<PersonContext>(options => options.UseSqlServer("Server=AIDA\\SQLEXPRESS;Database=IntroductionToAsp;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;"));
+*/
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
